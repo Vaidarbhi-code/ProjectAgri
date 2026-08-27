83e56f660f5ad4822b760de24147f1bd44d06ded
Vaidarbhi-code <vaidarbhibhandakkar@gmail.com>
2026-08-27 14:21:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -21,6 +21,7 @@
    FIREBASE CONFIGURATION
 ============================================================ */
 
+const BACKEND_BASE_URL = "http://127.0.0.1:5000";
 const firebaseConfig = {
     apiKey: "AIzaSyAuIbj5ajXbSu1_txFSJSLViAGcc1DBgHY",
     authDomain: "kopargaonproject.firebaseapp.com",
